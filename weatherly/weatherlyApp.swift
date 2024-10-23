ca816dff15f68daf6d9ebff1fc06e195c1a745ac
Sergio Santos <sergiorobsantos@gmail.com>
2024-10-23 14:57:50 -0300

    add initial project

---
+++ weatherly/weatherlyApp.swift
@@ -0,0 +1,17 @@
+//
+//  weatherlyApp.swift
+//  weatherly
+//
+//  Created by Sergio Santos on 23/10/24.
+//
+
+import SwiftUI
+
+@main
+struct weatherlyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ weatherlyTests/weatherlyTests.swift
@@ -0,0 +1,17 @@
+//
+//  weatherlyTests.swift
+//  weatherlyTests
+//
+//  Created by Sergio Santos on 23/10/24.
+//
+
+import Testing
+@testable import weatherly
+
+struct weatherlyTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
