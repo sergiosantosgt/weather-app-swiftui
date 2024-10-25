916770b1af175bb53dbdc8fc6702798b5983ce4e
Sergio Santos <sergiorobsantos@gmail.com>
2024-10-25 01:23:32 -0300

    add initial project

---
+++ Weatherly/WeatherlyApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherlyApp.swift
+//  Weatherly
+//
+//  Created by Sergio Santos on 25/10/24.
+//
+
+import SwiftUI
+
+@main
+struct WeatherlyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
